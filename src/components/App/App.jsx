081fa76858ec74b43b04d919c5017ffba3411a2a
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-16 15:43:21 +0300

    scr folder transfer

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { Contacts } from '../Contacts';
+import { Filter } from '../Filter';
+import { Forma } from '../Forma';
+import { ContainerForm } from './App.styled';
+
+export const App = () => {
+  return (
+    <ContainerForm>
+      <h1>Phonebook</h1>
+      <Forma />
+      <h2>Contacts</h2>
+      <Filter />
+      <Contacts />
+    </ContainerForm>
+  );
+};
---
+++ src/components/Forma/Forma.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { Input, FormBtn } from "./Forma.styled";
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact } from 'redux/sliceContact';
+import { nanoid } from 'nanoid';
+
+export const Forma = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const contactsValue = useSelector(state => state.contacts);
+  const dispatch = useDispatch();
+
+  const addContacts = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    dispatch(addContact(contact));
+  };
+
+  const hendleSubmit = event => {
+    event.preventDefault();
+    const nameContacts = contactsValue.map(el => el.name.toLowerCase());
+    if (nameContacts.includes(name.toLowerCase())) {
+      alert(`${name} is in your contacts`);
+    } else {
+      addContacts(name, number);
+      reset();
+    }
+  };
+
+  const hendleNameTelChange = event => {
+    const { name, value } = event.currentTarget;
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <>
+      <form onSubmit={hendleSubmit}>
+        <label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            pattern="^[A-Za-z\u0080-\uFFFF ']+$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={hendleNameTelChange}
+          />
+        </label>
+
+        <label>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            placeholder="Number"
+            pattern="^(\+?[0-9.\(\)\-\s]*)$"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={hendleNameTelChange}
+          />
+        </label>
+        <FormBtn type="submit">Add contact</FormBtn>
+      </form>
+    </>
+  );
+};
